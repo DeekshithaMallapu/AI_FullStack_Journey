ff8d846804a4008b950746df17dacdde27046df7
Mallapu Deekshitha <deekshithamallapu321@gmail.com>
2025-12-29 14:09:11 +0530

    Day 2: python basics, data types and operators

---
+++ day2_data_types.py
@@ -0,0 +1,10 @@
+age=19
+height=5.6
+name="DEEKSHITHA"
+is_student=True
+
+
+print(age,type(age))
+print(height,type(height))
+print(name,type(name))
+print(is_student,type(is_student))
---
+++ day2_operators.py
@@ -0,0 +1,23 @@
+a=10
+b=3
+
+print("ADDITION:",a+b)
+print("SUBRACTION:",a-b)
+print("MULTIPLICATION",a*b)
+print("DIVISION:",a/b)
+print("modulus:",a%b)
+print("power:",a**b)
+
+#comparision operators
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+
+#logical operators
+x=True
+y=False
+
+print(x and y)
+print(x or y)
+print(not x)
